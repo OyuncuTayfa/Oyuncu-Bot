fd53027494fc6b53d67c13a01b4c2e772f35edc6
OyuncuTayfa <44439969+OyuncuTayfa@users.noreply.github.com>
2020-09-18 15:44:05 +0300

    Create öneri.js

---
+++ komutlar/öneri.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+
+
+exports.run = function(client, message, args) {
+
+  var öneri = args.slice(0).join(' ');
+  var guildID = "499546891843928064";
+  var channelID = "756494706510135307";
+  
+  İF(!öneri) return message.reply("Bir mesaj belirtin! Doğru kullanımı: **?öneri <mesaj>");
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["öner"],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'öner', 
+  description: "Bot hakkındaki önerilerinizi bot sahibine ulaştırır",
+  usage: 'öneri <mesaj>'
+};
