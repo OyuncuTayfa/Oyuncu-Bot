12f3345fe6b9173b442aa15fa4728de08b144ae8
OyuncuTayfa <44439969+OyuncuTayfa@users.noreply.github.com>
2020-09-18 16:01:56 +0300

    Update öneri.js

--- komutlar/öneri.js
+++ komutlar/öneri.js
@@ -6,11 +6,12 @@ exports.run = function(client, message, args) {
   var öneri = args.slice(0).join(' ');
   var guildID = "499546891843928064";
   var channelID = "756494706510135307";
+  var kullanici = message.author
   
   if(!öneri){
     return message.reply('Bir mesaj belirtin! **Doğru Kullanım**: -öneri <mesaj>');
   } else{
-    client.guilds.get(guildID).channels.get(channelID).send(öneri);
+    client.guilds.get(guildID).channels.get(channelID).send(kullanici + öneri);
   };
     
   
