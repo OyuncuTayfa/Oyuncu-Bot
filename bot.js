5d02029fab7339cfd8c3ee6c6b970b5b3cb38557
OyuncuTayfa <44439969+OyuncuTayfa@users.noreply.github.com>
2020-12-21 10:48:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -113,8 +113,8 @@ client.elevation = message => {
   if(!message.guild) {
 	return; }
   let permlvl = 0;
-  if (message.member.permissions.has("BAN_MEMBERS")) permlvl = 2;
-  if (message.member.permissions.has("ADMINISTRATOR")) permlvl = 3;
+  if (message.member.perms.has("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.perms.has("ADMINISTRATOR")) permlvl = 3;
   if (message.author.id === ayarlar.sahip) permlvl = 4;
   return permlvl;
 };
