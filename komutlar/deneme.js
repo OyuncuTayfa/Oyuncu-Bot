5eb38241549d330258afbf3341e18a6506678971
OyuncuTayfa <44439969+OyuncuTayfa@users.noreply.github.com>
2020-12-22 09:34:14 +0300

    Create deneme.js

---
+++ komutlar/deneme.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+
+
+exports.run = function(client, message) {
+
+    var embed = new Discord.RichEmbed()
+    .addField("Deneme : ", "sa" , "as" , "hg" , "hb" , "deneme")
+    message.channel.send(embed);
+  };
+
+};
+
+exports.conf = {
+  enabled: true, 
+  guildOnly: false, 
+  aliases: ['ping','p'],
+  permLevel: 0 
+};
+
+exports.help = {
+  name: 'ping', 
+  description: 'Botun pingini gösterir', 
+  usage: 'ping' 
+};
