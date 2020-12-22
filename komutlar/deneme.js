edadaa64a49a5e375474a4211fd327ba99942468
OyuncuTayfa <44439969+OyuncuTayfa@users.noreply.github.com>
2020-12-22 10:08:43 +0300

    Update deneme.js

--- komutlar/deneme.js
+++ komutlar/deneme.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 exports.run = function(client, message) {
 
     var embed = new Discord.RichEmbed()
-    .setColor(0xffd700)
+    .setColor(0x549fff)
     .addField("Deneme", "sa")
     .addField("as" , "hg")
     .addField("hb" , "deneme")
