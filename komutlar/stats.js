c945e0777853efb7e7a94f7f8004fcbddb461992
OyuncuTayfa <44439969+OyuncuTayfa@users.noreply.github.com>
2020-12-23 09:36:19 +0300

    Update stats.js

--- komutlar/stats.js
+++ komutlar/stats.js
@@ -2,17 +2,19 @@ const { MessageEmbed } = require('discord.js')
 const moment = require('moment')
 require('moment-duration-format')
 const os = require('os')
-module.exports = {
-  kod: "stats",
-  async run (client, message) {
-    const embed = new MessageEmbed()
-    .setTitle('İstatistik')
-    .addField('Kullanıcı Sayısı', client.users.cache.size)
-    .addField('Sunucu Sayısı', client.guilds.cache.size)
-    .addField('Kanal Sayısı', client.channels.cache.size)
-    message.channel.send(embed)
+exports.run = function(client, message) {
+  module.exports = {
+    kod: "stats",
+    async run (client, message) {
+      const embed = new MessageEmbed()
+      .setTitle('İstatistik')
+      .addField('Kullanıcı Sayısı', client.users.cache.size)
+      .addField('Sunucu Sayısı', client.guilds.cache.size)
+      .addField('Kanal Sayısı', client.channels.cache.size)
+      message.channel.send(embed)
+    }
   }
-}
+};
 
 exports.conf = {
   enabled: true, 
