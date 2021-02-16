8059b4e8bd9b2720646b9e27ec94c49062a106b8
OyuncuTayfa <44439969+OyuncuTayfa@users.noreply.github.com>
2021-02-16 09:26:56 +0300

    Update taşkağıtmakas.js

--- komutlar/taşkağıtmakas.js
+++ komutlar/taşkağıtmakas.js
@@ -1,31 +1,21 @@
 const Discord = require('discord.js');
 const { Command } = require('discord.js-commando');
 const { stripIndents } = require('common-tags');
-const cevaplar = [':fist:',':hand_splayed:',':v:'];
+const cevaplar = [':fist:',':raised_hand:',':v:'];
 
 exports.run = function(client, message, args) {
 
     const cevap = args.slice(0).join(' ');
 
 	var cevap1 = cevaplar[Math.floor(Math.random() * cevaplar.length)];
-    if (!cevap) return message.reply('Lütfen bir sembol seç. **Doğru Kullanım** : -tkm <:fist:,:hand_splayed:,:v:>')
-    message.channel.send(stripIndents`
-     __SEN__  __BEN__
-    ${cevap} : ${cevap1}
-    `)
-	if (cevap === cevap1) {
-		message.channel.send(stripIndents`
-         __SEN__   __BEN__
-        ${cevap} : ${cevap1}
-		Eyvah Berabere Kaldık.
-		`); 
-	} else if (cevap === ':v:' && cevap1 === ':hand_splayed:') {
-        message.channel.send(stripIndents`
-         __SEN__   __BEN__
-        ${cevap} : ${cevap1}
-        Makas Kağıdı keser sen kazandın.
-		`);	
-	}
+    if (!cevap) return message.reply('Lütfen bir sembol seç. **Doğru Kullanım** : -tkm <:fist:,:raised_hand:,:v:>')
+if (cevap === ':raised_hand:') {
+	
+} else if (cevap === ':fist:') {
+
+} else if (cevap === ':v:') {
+
+} else message.replt('Lütfen **geçerli** bir sembol seç. **Doğru Kullanım** : -tkm <:fist:,:raised_hand:,:v:>')
 
 };
 
@@ -41,3 +31,25 @@ exports.help = {
   description: 'Taş Kağıt Makas oyunu oynatır',
   usage: 'taşkağıtmakas'
 };
+
+
+
+
+
+//message.channel.send(stripIndents`
+//     __SEN__  __BEN__
+//    ${cevap} : ${cevap1}
+//    `)
+//	if (cevap === cevap1) {
+//		message.channel.send(stripIndents`
+//         __SEN__   __BEN__
+//        ${cevap} : ${cevap1}
+//		Eyvah Berabere Kaldık.
+//		`); 
+//	} else if (cevap === ':v:' && cevap1 === ':hand_splayed:') {
+//        message.channel.send(stripIndents`
+//         __SEN__   __BEN__
+//        ${cevap} : ${cevap1}
+//        Makas Kağıdı keser sen kazandın.
+//		`);	
+//	}
